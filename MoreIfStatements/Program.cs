2e6d76e5774a4f93c42c687fe0996897cabc98ac
melisacevik <celissamevik@gmail.com>
2022-12-29 09:16:23 +0300

    MoreIfStatementsExp

---
+++ MoreIfStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetMax(2, 10));
+        Console.ReadLine();
+    }
+
+    static int GetMax(int num1, int num2)
+    {
+
+        #region basic exp.
+
+        int result;
+
+        if (num1 > num2)              // bir koşul ya doğrudur ya yanlıştır.
+        {
+            result = num1;            // eğer num1 num2den büyükse; sonuç num1'dir. max'ı alıyoruz.
+        }
+        else
+        {
+            result = num2;
+        }
+
+        return result;              //işlem bittikten sonra void Main kısmına geç ve istediğin iki sayısı yazdır.
+
+
+
+
+        #endregion
+
+    }
+
+}
